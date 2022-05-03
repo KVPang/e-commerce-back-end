d3753be831d820afd3ccef183d4229e60b48815a
Kristina Pang <kvypang@gmail.com>
2022-05-03 15:53:54 -0700

    category and product routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,44 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [Product]
+
+
+  }).then(db => {res.json(db)})
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {id:req.params.id}, 
+    include: [Product]
+
+}).then(db => {res.json(db)})
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(
+    req.body
+
+  ).then(db => {res.json(db)})
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body,{
+    where: {id: req.params.id},
+
+  }).then(db => {res.json(db)})
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {id: req.params.id}
+  }).then(db => {res.json(db)})
 });
 
 module.exports = router;

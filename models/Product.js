ca3fb1d223e3e41648a9c64f2f1e657df8fa3c03
Kristina Pang <kvypang@gmail.com>
2022-05-03 08:52:13 -0700

    sequelize sync, category and product models

--- models/Product.js
+++ models/Product.js
@@ -8,8 +8,36 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
+  // define columns
   {
-    // define columns
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+    },
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+    },
+  price: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+    validate: {
+      isDecimal: true }
+    },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: {
+      isNumeric: true }
+    },
+  category_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'category',
+      key: 'id' }
+    }
   },
   {
     sequelize,
@@ -17,7 +45,6 @@ Product.init(
     freezeTableName: true,
     underscored: true,
     modelName: 'product',
-  }
-);
+});
 
 module.exports = Product;
